d70c68d73d0b7ec7c9f8e83669bfc3fc99dfe6a4
A-Salty-Fish <1309032417@qq.com>
2020-05-21 19:58:48 +0800

    Finish

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -15,59 +15,19 @@ const int MaxPCBNum = 5;//进程数
 
 class PCB{
     string name;//进程名
-    // PCB * nextPCB;//下一个进程PCB的地址
     int time;//要求运行时间
     int priority;//优先数
     bool status;//状态 0为完成 1为就绪
-    //***其他内容
+
 public:
+    bool Run();//运行 返回是否成功运行
+    bool Input();//输入PCB信息 返回是否成功创建
     PCB(){name="none";time=0;priority=0;status= false;}//默认构造函数
     PCB(string n,int t,int p,bool s):name(n),time(t),priority(p),status(s){}//构造函数
     int GetPriority(){return priority;}//返回优先数
     bool GetStatus(){return status;}//返回状态
     bool operator<(PCB pcb){return priority>pcb.priority;}//重载小于以使用sort 因为从大到小排序 所以返回大与
     bool operator==(PCB pcb){return priority==pcb.priority;}//重载等于以使用sort
-    bool Run()//运行 返回是否成功运行
-    {
-        if (time<1||!status)//参数错误
-            return false;
-        else
-        {
-            //***运行
-            time--;
-            priority--;
-            if (time==0) status = false;//更新状态
-            cout<<name<<" is running."<<endl;
-            return true;
-        }
-    }
-    bool Input()//输入PCB信息 返回是否成功创建
-    {
-        string n;//name
-        int t,p;//time priority
-        colour(BLUE);cout<<"Please enter the name of Process:\n";
-        cin >> n;
-        name = n;
-        colour(BLUE);cout<<"Please enter the time:\n";
-        cin >> t;
-        colour(BLUE);cout<<"Please enter the priority:\n";
-        cin>>p;
-        if (t<0||p<0)//非法输入
-        {
-            colour(RED); cout<<"Invalid Input."<<endl;
-            return false;//创建失败
-        }
-        else
-        {
-            priority=p;
-            time=t;
-            status=true;
-            cout<<"The Process you created:"<<endl;//打印刚输入的进程信息
-            colour(YELLOW); cout<<"Name"<<'\t'<<"Time"<<'\t'<<"Priority"<<'\t'<<"status"<<endl;
-            Show(PURPLE);
-            return true;//创建成功
-        }
-    }
     void Show(int color = GREEN)//打印进程信息
     {
         colour(color);
@@ -75,78 +35,129 @@ public:
     }
 };
 
+bool PCB::Run()
+{
+    if (time<1||!status)//参数错误
+        return false;
+    else
+    {
+        //***运行
+        time--;
+        priority--;
+        if (time==0) status = false;//更新状态
+        cout<<name<<" is running."<<endl;
+        return true;
+    }
+}
+
+bool PCB::Input()
+{
+    string n;//name
+    int t,p;//time priority
+    colour(BLUE);cout<<"Please enter the name of Process:\n";
+    cin >> n;
+    name = n;
+    colour(BLUE);cout<<"Please enter the time:\n";
+    cin >> t;
+    colour(BLUE);cout<<"Please enter the priority:\n";
+    cin>>p;
+    if (t<0||p<0)//非法输入
+    {
+        colour(RED); cout<<"Invalid Input."<<endl;
+        return false;//创建失败
+    }
+    else
+    {
+        priority=p;
+        time=t;
+        status=true;
+        cout<<"The Process you created:"<<endl;//打印刚输入的进程信息
+        colour(YELLOW); cout<<"Name"<<'\t'<<"Time"<<'\t'<<"Priority"<<'\t'<<"status"<<endl;
+        Show(PURPLE);
+        return true;//创建成功
+    }
+}
+
 class OS{
     deque<PCB> ReadyDeque;//就绪队列
     deque<PCB> EndDeque;//完成队列
 public:
     OS(){}
-    void Initial()//初始化
-    {
-
-    }
-    void Input()//输入信息
-    {
-        for (int i=0;i<MaxPCBNum;i++)
-        {
-            PCB pcb;//创建进程
-            colour(PINK);cout<<"Process "<< i+1 <<endl;
-            if (pcb.Input())//如果创建合法则添加入就绪队列
-            {
-                ReadyDeque.push_back(pcb);
-            }
-            else//创建不合法
-            {
-                colour(RED);cout<<"Please enter again."<<endl;
-                i--;
-            }
-        }
-        sort(ReadyDeque.begin(), ReadyDeque.end());//创建完后按优先数从大到小的顺序排列
-    }
-    void Show(bool ready)//打印队列进程信息 ready为true打印就绪队列 false打印完成队列
-    {
-        if (ready) //就绪队列
-        {
-            colour(GREEN);cout << "Ready:" << endl;
-            colour(YELLOW);cout<<"Name"<<'\t'<<"Time"<<'\t'<<"Priority"<<'\t'<<"status"<<endl;
-            for (auto i = ReadyDeque.begin(); i != ReadyDeque.end(); i++)
-                i->Show();
-        }
-        else//完成队列
-        {
-            colour(PINK);cout << "End:" << endl;
-            colour(YELLOW);cout<<"Name"<<'\t'<<"Time"<<'\t'<<"Priority"<<'\t'<<"status"<<endl;
-            for(auto i = EndDeque.begin(); i != EndDeque.end(); i++)
-                i->Show();
-        }
-
-    }
-
-    void Run()//运行就绪队列中的进程
-    {
-        getchar();//吞入上一个回车
-        int num = 1;//记录轮数
-        while (!ReadyDeque.empty())//就绪队列非空
-        {
-            colour(RED);cout << "Turn " << num <<": ";
-            ReadyDeque.begin()->Run();//运行优先度最高的进程 此进程优先级和时间均减一
-            if (ReadyDeque.begin()->GetStatus() == false)//队首进程完成则出队并添加入完成队列
-            {
-                EndDeque.push_back(ReadyDeque[0]);
-                ReadyDeque.pop_front();
-            }
-            sort(ReadyDeque.begin(),ReadyDeque.end());//再次排序就绪队列
-            Show(true);//打印排序后的就绪队列
-            if (!EndDeque.empty())//如果完成队列非空
-                Show(false);//打印完成队列
-            getchar();//等待输入后继续
-            num++;//轮数加一
-        }
-        cout<<"All end.";//全部完成
-    }
+    void Initial();//初始化
+    void Input();//输入信息
+    void Run();//运行就绪队列中的进程
+    void Show(bool ready);//打印队列进程信息 ready为true打印就绪队列 false打印完成队列
 };
+
+void OS::Initial() {
+
+}
+
+void OS::Input() {
+    for (int i=0;i<MaxPCBNum;i++)
+    {
+        PCB pcb;//创建进程
+        colour(PINK);cout<<"Process "<< i+1 <<endl;
+        if (pcb.Input())//如果创建合法则添加入就绪队列
+        {
+            ReadyDeque.push_back(pcb);
+        }
+        else//创建不合法
+        {
+            colour(RED);cout<<"Please enter again."<<endl;
+            i--;
+        }
+    }
+    sort(ReadyDeque.begin(), ReadyDeque.end());//创建完后按优先数从大到小的顺序排列
+}
+
+void OS::Show(bool ready) {
+    if (ready) //就绪队列
+    {
+        colour(GREEN);cout << "Ready:" << endl;
+        colour(YELLOW);cout<<"Name"<<'\t'<<"Time"<<'\t'<<"Priority"<<'\t'<<"status"<<endl;
+        for (auto i = ReadyDeque.begin(); i != ReadyDeque.end(); i++)
+            i->Show();
+    }
+    else//完成队列
+    {
+        colour(PINK);cout << "End:" << endl;
+        colour(YELLOW);cout<<"Name"<<'\t'<<"Time"<<'\t'<<"Priority"<<'\t'<<"status"<<endl;
+        for(auto i = EndDeque.begin(); i != EndDeque.end(); i++)
+            i->Show();
+    }
+}
+
+void OS::Run() {
+    getchar();//吞入上一个回车输入
+    int num = 1;//记录轮数
+    while (!ReadyDeque.empty())//就绪队列非空
+    {
+        colour(RED);cout << "Turn " << num <<": ";
+        ReadyDeque.begin()->Run();//运行优先度最高的进程 此进程优先级和时间均减一
+        if (ReadyDeque.begin()->GetStatus() == false)//队首进程完成则出队并添加入完成队列
+        {
+            EndDeque.push_back(ReadyDeque[0]);
+            ReadyDeque.pop_front();
+        }
+        else//否则再次加入队列尾部
+        {
+            ReadyDeque.push_back(ReadyDeque[0]);
+            ReadyDeque.pop_front();
+        }
+        sort(ReadyDeque.begin(),ReadyDeque.end());//再次排序就绪队列
+        Show(true);//打印排序后的就绪队列
+        if (!EndDeque.empty())//如果完成队列非空
+            Show(false);//打印完成队列
+        getchar();//等待输入后继续
+        num++;//轮数加一
+    }
+    cout<<"All end.";//全部完成
+}
 
 int main() {
     OS os;
+    os.Initial();
     os.Input();
     os.Show(true);
     os.Run();
--- Project2/main.cpp
+++ Project2/main.cpp
@@ -37,119 +37,128 @@ class LinkList{//分区链
     Node * head;//链表头结点
 public:
     LinkList(){head=new Node(-1,-1);}
-    void AddNode(int ad,int mem)//在尾部添加节点
-    {
-        Node * node=head;
-        while (node->next!= nullptr)
-            node=node->next;//找到最后一个节点
-        node->next=new Node(ad,mem);
-    }
-    Node * FindMem(int mem)//找到第一个空间大于mem的节点
-    {
-        Node * node = head->next;
-        while (node!= nullptr)
-        {
-            if (node->GetMem()>=mem)//如果空间大于要求
-            {
-                return node;//返回该节点
-            }
-            node=node->next;
-        }
-        return nullptr;//没找到返回空指针
-    }
-    Node * FindNum(int num)//返回第num个节点
-    {
-        int i=1;
-        Node * node = head->next;
-        while (node!= nullptr)
-        {
-            if (i==num) return node;
-            node=node->next;
-            i++;
-        }
-        return nullptr;
-    }
-    Node * FindAddr(int addr)//返回地址为addr的节点
-    {
-        Node * node = head->next;
-        while (node!= nullptr)
-        {
-            if (node->GetAddr()==addr) return node;
-            node=node->next;
-        }
-        return nullptr;
-    }
-    void Delete(Node * Dnode)//删除一个节点
-    {
-        Node* node= head;
-        while (node!= nullptr)
-        {
-            if (node->next==Dnode)
-            {
-                node->next = Dnode->next;
-                delete Dnode;
-            }
-            node=node->next;
-        }
-    }
-    void Show()
-    {
-        int i=0;//计数项
-        Node * node=head->next;
-        colour(GREEN);cout<<"Num"<<"\t"<<"Address"<<"\t"<<"Size"<<endl;
-        while (node != nullptr)//遍历链表
-        {
-            i++;
-            node->Show(i);
-            node=node->next;
-        }
-    }
-    void Sort()//从低地址向高地址排序 地址交换 非值交换
-    {
-        Node * One = head->next;//递增节点1
-        Node * Two;//递增节点2
-        Node * MinNode;//每轮内层循环中地址最小的节点
-        while(One!= nullptr)
-        {
-            Two = One;
-            MinNode = One;
-            while(Two!= nullptr)
-            {
-                if (Two->GetAddr()<MinNode->GetAddr())
-                {
-                    MinNode=Two;
-                }
-                Two=Two->next;
-            }
-            if (MinNode!=One)//找到了错位最小值
-            {
-                MinNode->Swap(One);//交换节点值
-            }
-            One=One->next;
-        }
-    }
-    void Merge(Node * node)//用于合并空闲分区前后的空闲分区
-    {
-        if (node== nullptr) return;
-        Node*FrontNode=head;//前节点
-        while(FrontNode->next!=node && FrontNode!= nullptr)//找到node前面的节点
-            FrontNode=FrontNode->next;
-        Node*BackNode=node->next;//后节点
-        if (BackNode!=nullptr)//尝试合并后面与当前的节点
-        {
-            if (node->GetAddr()+node->GetMem()==BackNode->GetAddr())//满足合并条件
-            {
-                node->Merge(BackNode->GetMem());//合并节点
-                Delete(BackNode);//删除后节点
-            }
-        }
-        if (FrontNode->GetAddr()+FrontNode->GetMem()==node->GetAddr())//满足合并前节点与当前节点
-        {
-            FrontNode->Merge(node->GetMem());//合并节点
-            Delete(node);//删除当前节点
-        }
-    }
+    void AddNode(int ad,int mem);//在尾部添加节点
+    Node * FindMem(int mem);//找到第一个空间大于mem的节点
+    Node * FindNum(int num);//返回第num个节点
+    Node * FindAddr(int addr);//返回地址为addr的节点
+    void Delete(Node * Dnode);//删除一个节点
+    void Show();//打印信息
+    void Sort();//从低地址向高地址排序 地址交换 非值交换
+    void Merge(Node * node);//用于合并空闲分区前后的空闲分区
 };
+
+void LinkList::AddNode(int ad, int mem)
+{
+    Node * node=head;
+    while (node->next!= nullptr)
+        node=node->next;//找到最后一个节点
+    node->next=new Node(ad,mem);
+}
+
+Node *LinkList::FindMem(int mem)     {
+    Node * node = head->next;
+    while (node!= nullptr)
+    {
+        if (node->GetMem()>=mem)//如果空间大于要求
+        {
+            return node;//返回该节点
+        }
+        node=node->next;
+    }
+    return nullptr;//没找到返回空指针
+}
+
+Node *LinkList::FindNum(int num) {
+    int i=1;
+    Node * node = head->next;
+    while (node!= nullptr)
+    {
+        if (i==num) return node;
+        node=node->next;
+        i++;
+    }
+    return nullptr;
+}
+
+Node *LinkList::FindAddr(int addr) {
+    Node * node = head->next;
+    while (node!= nullptr)
+    {
+        if (node->GetAddr()==addr) return node;
+        node=node->next;
+    }
+    return nullptr;
+}
+
+void LinkList::Delete(Node *Dnode) {
+    Node* node= head;
+    while (node!= nullptr)
+    {
+        if (node->next==Dnode)
+        {
+            node->next = Dnode->next;
+            delete Dnode;
+        }
+        node=node->next;
+    }
+}
+
+void LinkList::Show()     {
+    int i=0;//计数项
+    Node * node=head->next;
+    colour(GREEN);cout<<"Num"<<"\t"<<"Address"<<"\t"<<"Size"<<endl;
+    while (node != nullptr)//遍历链表
+    {
+        i++;
+        node->Show(i);
+        node=node->next;
+    }
+}
+
+void LinkList::Sort() {
+    Node * One = head->next;//递增节点1
+    Node * Two;//递增节点2
+    Node * MinNode;//每轮内层循环中地址最小的节点
+    while(One!= nullptr)
+    {
+        Two = One;
+        MinNode = One;
+        while(Two!= nullptr)
+        {
+            if (Two->GetAddr()<MinNode->GetAddr())
+            {
+                MinNode=Two;
+            }
+            Two=Two->next;
+        }
+        if (MinNode!=One)//找到了错位最小值
+        {
+            MinNode->Swap(One);//交换节点值
+        }
+        One=One->next;
+    }
+}
+
+void LinkList::Merge(Node *node) {
+    if (node== nullptr) return;
+    Node*FrontNode=head;//前节点
+    while(FrontNode->next!=node && FrontNode!= nullptr)//找到node前面的节点
+        FrontNode=FrontNode->next;
+    Node*BackNode=node->next;//后节点
+    if (BackNode!=nullptr)//尝试合并后面与当前的节点
+    {
+        if (node->GetAddr()+node->GetMem()==BackNode->GetAddr())//满足合并条件
+        {
+            node->Merge(BackNode->GetMem());//合并节点
+            Delete(BackNode);//删除后节点
+        }
+    }
+    if (FrontNode->GetAddr()+FrontNode->GetMem()==node->GetAddr())//满足合并前节点与当前节点
+    {
+        FrontNode->Merge(node->GetMem());//合并节点
+        Delete(node);//删除当前节点
+    }
+}
 
 class OS{
     LinkList FreeLinkList;//空闲分区链
@@ -157,82 +166,85 @@ class OS{
 public:
     void Initial(){FreeLinkList.AddNode(0, MaxMemory);}//初始化
     OS(){}
-    void Input()//获取输入
-    {
-        bool cycle = true;//循环变量
-        while (cycle)
-        {
-            colour(PINK);cout<<"Free:"<<endl;
-            FreeLinkList.Show();//打印当前空闲分区信息
-            colour(PINK);cout<<"Alloc:"<<endl;
-            AllocLinkList.Show();//打印已分配信息
-            int input;
-            colour(PURPLE);cout<<"Enter 1 to allocate, 2 to recycle, others to quit:";
-            cin>>input;
-            switch (input)
-            {
-                case 1:Allocate();break;
-                case 2:Recycle();break;
-                default:cycle=false;break;
-            }
-        }
-    }
-    bool Allocate()//分配
-    {
-        colour(YELLOW);cout<<"Please enter the memory:";
-        int mem;cin>>mem;
-        if (mem<=0)//错误输入
-        {
-            colour(RED);cout<<"Invalid Input."<<endl;
-            return false;
-        }
-        else
-        {
-            Node * AllocNode = FreeLinkList.FindMem(mem);//找到最前面的满足要求的空闲节点
-            if(AllocNode!= nullptr)
-            {
-                AllocLinkList.AddNode(AllocNode->GetAddr(),mem);
-                AllocLinkList.Sort();
-                AllocNode->Allocate(mem);
-                if (AllocNode->GetMem()==0)//该空闲节点被分配完
-                    FreeLinkList.Delete(AllocNode);//移除该空闲节点
-            }
-            else//分配失败
-            {
-                colour(RED);cout<<endl<<"No enough mem."<<endl<<endl;
-                return false;
-            }
-        }
-        return true;
-    }
-    bool Recycle()
-    {
-        colour(YELLOW);cout<<"Please enter the num to recycle:";
-        int num;cin>>num;
-        if (num<=0)//错误输入
-        {
-            colour(RED);cout<<"Invalid Input."<<endl;
-            return false;
-        }
-        else
-        {
-            Node * node = AllocLinkList.FindNum(num);
-            if (node == nullptr)//没找到该节点
-            {
-                colour(RED);cout<<"Not Found."<<endl;
-                return false;
-            }
-            else//找到该节点
-            {
-                FreeLinkList.AddNode(node->GetAddr(),node->GetMem());//添加入空闲区
-                FreeLinkList.Sort();
-                FreeLinkList.Merge(FreeLinkList.FindAddr(node->GetAddr()));
-                AllocLinkList.Delete(node);//从分配区中移除
-            }
-        }
-        return true;
-    }
+    void Input();//获取输入
+    bool Allocate();//分配
+    bool Recycle();//回收
 };
+
+void OS::Input() {
+    bool cycle = true;//循环变量
+    while (cycle)
+    {
+        colour(PINK);cout<<"Free:"<<endl;
+        FreeLinkList.Show();//打印当前空闲分区信息
+        colour(PINK);cout<<"Alloc:"<<endl;
+        AllocLinkList.Show();//打印已分配信息
+        int input;
+        colour(PURPLE);cout<<"Enter 1 to allocate, 2 to recycle, others to quit:";
+        cin>>input;
+        switch (input)
+        {
+            case 1:Allocate();break;
+            case 2:Recycle();break;
+            default:cycle=false;break;
+        }
+    }
+}
+
+bool OS::Allocate() {
+    colour(YELLOW);cout<<"Please enter the memory:";
+    int mem;cin>>mem;
+    if (mem<=0)//错误输入
+    {
+        colour(RED);cout<<"Invalid Input."<<endl;
+        return false;
+    }
+    else
+    {
+        Node * AllocNode = FreeLinkList.FindMem(mem);//找到最前面的满足要求的空闲节点
+        if(AllocNode!= nullptr)
+        {
+            AllocLinkList.AddNode(AllocNode->GetAddr(),mem);
+            AllocLinkList.Sort();
+            AllocNode->Allocate(mem);
+            if (AllocNode->GetMem()==0)//该空闲节点被分配完
+                FreeLinkList.Delete(AllocNode);//移除该空闲节点
+        }
+        else//分配失败
+        {
+            colour(RED);cout<<endl<<"No enough mem."<<endl<<endl;
+            return false;
+        }
+    }
+    return true;
+}
+
+bool OS::Recycle() {
+    colour(YELLOW);cout<<"Please enter the num to recycle:";
+    int num;cin>>num;
+    if (num<=0)//错误输入
+    {
+        colour(RED);cout<<"Invalid Input."<<endl;
+        return false;
+    }
+    else
+    {
+        Node * node = AllocLinkList.FindNum(num);
+        if (node == nullptr)//没找到该节点
+        {
+            colour(RED);cout<<"Not Found."<<endl;
+            return false;
+        }
+        else//找到该节点
+        {
+            FreeLinkList.AddNode(node->GetAddr(),node->GetMem());//添加入空闲区
+            FreeLinkList.Sort();
+            FreeLinkList.Merge(FreeLinkList.FindAddr(node->GetAddr()));
+            AllocLinkList.Delete(node);//从分配区中移除
+        }
+    }
+    return true;
+}
 
 int main() {
     OS os;
